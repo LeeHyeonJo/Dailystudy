33ee211caa9f11a64c7580bae3cdba877fb7c114
hyeonjo <gogreen97@naver.com>
2023-08-09 00:07:34 +0900

    이현조: 0809 Lv10~

---
+++ 10-01/10-01.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[4][4] = {}; 
+
+	int num;
+	cin >> num; 
+
+	switch (num % 2) // 짝 홀 둘중 하나겠죠 
+	{
+
+		case 0:
+		{ 
+			// 짝수 
+			int a = 1;
+			for (int i = 0; i < 4; i++)
+			{
+				arr[i][i] = a++; 
+				// 00 11 22 33
+			}
+			break; 
+		}
+
+		default:
+		{
+			// 홀수겠지
+			int b = 1; 
+			for (int i = 0; i < 4; i++)
+			{
+				// 03 12 21 30
+				// 어쩌구 = 3-i; 
+				int right = 3 - i; 
+				arr[i][right] = b++; 
+			}
+		}
+
+	}
+
+	// 출력
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			cout << arr[y][x] << ' '; 
+		}
+		cout << endl; 
+	}
+}
---
+++ 10-02/10-02.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+void geta()
+{
+	char a; 
+	char b;
+	cin >> a >> b; 
+
+	// 아스키코드가 더 큰 값을 리턴합니다~ 
+
+	if ((int)a > (int)b)
+	{
+		cout << a << ' ' << "이놈의 아스키 값은 " << (int)a; 
+	}
+
+	else
+	{
+		cout << b << ' ' << "이놈의 아스키값은 " << (int)b; 
+	}
+}
+
+int main()
+{
+	geta();
+}
