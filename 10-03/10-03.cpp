55b62a1061a522fa54e53aafabeed1020c3b294d
hyeonjo <gogreen97@naver.com>
2023-08-11 22:21:21 +0900

    이현조: 0811 ~Lv10 진행중

--- 10-03/10-03.cpp
+++ 10-03/10-03.cpp
@@ -1,7 +1,74 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
+int arr[3][3] = {}; 
+int a = 10; 
+int b = 1; 
+
+void wow(int _num)
 {
 
+	switch (_num % 5)
+	{
+
+		case 1:
+		{
+
+			for (int y = 2; y >= 0; y--)
+			{
+				for (int x = 2; x >= 0; x--)
+				{
+					arr[x][y] = b++; 
+				}
+			}
+
+			break;
+		}
+		
+		case 2:
+		{
+			for (int y = 2; y >= 0; y--)
+			{
+				for (int x = 0; x < 3; x++)
+				{
+					arr[y][x] = b++;
+				}
+			}
+
+			break;
+		}
+
+		default:
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				for (int x = 0; x < 3; x++)
+				{
+					arr[x][y] = a++;
+				}
+			}
+		}
+
+	}
+}
+
+void printing()
+{
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			cout << arr[y][x] << ' ';
+		}
+		cout << endl; 
+	}
+}
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	wow(num); 
+	printing(); 
 }
--- 10-04/10-04.cpp
+++ 10-04/10-04.cpp
@@ -1,7 +1,42 @@
 ﻿#include <iostream>
 using namespace std;
 
+void printdata(int value)
+{
+	cout << value<<endl;
+}
+
+void even(int value)
+{
+	printdata(value * 2); 
+}
+
+void odd(int value)
+{
+	printdata(value - 10); 
+}
+
 int main()
 {
+	int a;
+	int b;
+	cin >> a >> b;
 
+	switch ((a / b)%2)
+	{
+		case 0:
+		{
+			// 짝수일때 
+			even(a/b); 
+			break; 
+		}
+
+		default:
+		{
+			// 홀수일때 
+			odd(a/b);
+		}
+	}
+
+	printdata(a+b); 
 }
